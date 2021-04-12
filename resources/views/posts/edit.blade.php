663e83ba4e3cbbc1eae49d05c61ec6be6fafc19b
杉山俊介 <sugiyamashunsuke@sugiyamashunsukenoMacBook-puro.local>
2021-04-12 18:15:33 +0900

    fix edit layouts

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -7,12 +7,23 @@
 <!-- @yield('content')に埋め込まれます -->
 @section('content')
 
-    <form action="/posts/{{ $post->id }}" method="POST">
-        {{ csrf_field() }}
-        <input type="hidden" name="_method" value="PUT">
-        <input type="text" name="title" value="{{ $post->title }}">
-        <input type="text" name="content" value="{{ $post->content }}">
-        <input type="text">
-    </form>
+<h1>Editing Post</h1>
+
+<form action="/posts/{{ $post->id }}" method="POST">
+    {{ csrf_field() }}
+    <input type="hidden" name="_method" value="PUT">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Title</label>
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{ $post->title }}">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Content</label>
+        <textarea class="form-control" name="content">{{ $post->content }}</textarea>
+    </div>
+    <button type="submit" class="btn btn-outline-primary">Submit</button>
+</form>
+
+<a href="/posts/{{ $post->id }}">Show</a>
+<a href="/posts"></a>
 
 @endsection
